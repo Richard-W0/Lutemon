881f6c754dd487990252c202c7f02c7fec66d8d9
silvanestia <karanta.sade@gmail.com>
2025-04-26 23:26:32 +0300

    fixed a catastrophic error

--- app/src/main/java/com/example/lutemon/BattleAcctivity.java
+++ app/src/main/java/com/example/lutemon/BattleAcctivity.java
@@ -14,11 +14,7 @@ public class BattleAcctivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_battle_acctivity);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
+
+    // turha!! poista!!!
 }
