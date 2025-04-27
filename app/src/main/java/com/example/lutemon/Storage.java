24d00939697a5c6265e817ae6877288e65ef38b3
Richard-W0 <rikuwong@hotmail.fi>
2025-04-27 05:51:00 +0300

    fixed storage to be singleton and not delete itself

--- app/src/main/java/com/example/lutemon/Storage.java
+++ app/src/main/java/com/example/lutemon/Storage.java
@@ -2,23 +2,35 @@ package com.example.lutemon;
 import java.util.HashMap;
 
 public class Storage {
-    private HashMap<Integer, Lutemon> Lutemons;
+    private static Storage storage = null;
+
     private String name;
+    private HashMap<Integer, Lutemon> lutemons;
 
-    public Storage(String name){
-        this.name = name;
-        this.Lutemons = new HashMap<>();
-    }
-    public void addLutemon(Lutemon lutemon){
-        Lutemons.put(lutemon.getId(), lutemon); // voisiko lutemonin luoda suoraan tänne?
-    }
-
-    public Lutemon getLutemon(int id){
-        return Lutemons.get(id);
+    // Private constructor: prevents creating multiple storages
+    private Storage() {
+        this.name = "Lutemon Storage";
+        this.lutemons = new HashMap<>();
     }
 
-    public void listLutemons(){
-        for (Lutemon lutemon : Lutemons.values()){
+    // Static method to get the single instance
+    public static Storage getInstance() {
+        if (storage == null) {
+            storage = new Storage();
+        }
+        return storage;
+    }
+
+    public void addLutemon(Lutemon lutemon) {
+        lutemons.put(lutemon.getId(), lutemon);
+    }
+
+    public Lutemon getLutemon(int id) {
+        return lutemons.get(id);
+    }
+
+    public void listLutemons() {
+        for (Lutemon lutemon : lutemons.values()) {
             System.out.println("ID: " + lutemon.getId() + ", Name: " + lutemon.getName() + ", Color: " + lutemon.getColor());
         }
     }
